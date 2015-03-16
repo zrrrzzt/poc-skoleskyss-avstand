1f24eb1c0c23aa6b74d0ede503be15b28404017e
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-03-16 14:23:27 +0100

    Adds map with directions

--- elements/avstandsresultat.js
+++ elements/avstandsresultat.js
@@ -1,9 +1,15 @@
 'use strict';
 
 var React = require('react');
+var startUrl= 'https://www.google.com/maps/embed/v1/directions?key=AIzaSyAu4oe2bKCeP4AnAyo78KL_XZvrS-WVIcw&mode=walking&origin=';
 
 var AvstandsResultat = React.createClass({
+
   render: function() {
+    var emptyUrl = '';
+    var embedUrl = startUrl + this.props.result.origin + '&destination=' + this.props.result.destination;
+    var mapUrl = this.props.result.origin ? embedUrl:emptyUrl;
+
     return (
       <div>
         <h2>{this.props.result.distance}</h2>
@@ -11,6 +17,7 @@ var AvstandsResultat = React.createClass({
           {this.props.result.origin}<br />
           {this.props.result.destination}
         </p>
+        <iframe width="100%" height="450" frameBorder="0" src={mapUrl}></iframe>
       </div>
     );
   }
